93c8b2b5daccd5e01114172b535669bb66de8b3c
jaime <A20203943@idat.edu.pe>
2023-09-16 21:22:04 -0500

    primer commit

---
+++ optimedica/src/main/java/com/optimedica/OptimedicaApplication.java
@@ -0,0 +1,13 @@
+package com.optimedica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimedicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimedicaApplication.class, args);
+	}
+
+}
---
+++ optimedica/src/test/java/com/optimedica/OptimedicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optimedica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimedicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
