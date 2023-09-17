5eeb4a9d37f4b654b4689fd6a6c6ba5b7fc99e3c
jaime <A20203943@idat.edu.pe>
2023-09-16 22:57:55 -0500

    crud completo

--- optimedica/src/main/java/com/optimedica/controller/MarcaController.java
+++ optimedica/src/main/java/com/optimedica/controller/MarcaController.java
@@ -5,9 +5,8 @@ import com.optimedica.service.MarcaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,11 +17,41 @@ public class MarcaController {
     @Autowired
     private MarcaService service;
 
-    @GetMapping("/lista")
+    @GetMapping("/listar")
     public ResponseEntity<List<Marca>> listarMarcas(){
         List<Marca> marc = service.listarMarca();
         return new ResponseEntity<List<Marca>>(marc, HttpStatus.OK);
+    }
 
+    @PostMapping("/agregar")
+    public ResponseEntity<Marca> agragarMarca(@Validated @RequestBody Marca marca){
+        Marca marca1 = service.guardar(marca);
+        return new ResponseEntity<Marca>(marca1, HttpStatus.OK);
+    }
+
+    @PutMapping("/actualizar")
+    public ResponseEntity<Marca> actualizarMarca(@Validated @RequestBody Marca marca){
+        Marca marca2 = service.actualizar(marca);
+        return new ResponseEntity<Marca>(marca2, HttpStatus.OK);
+    }
+
+    @GetMapping("/listarId/{id_marca}")
+    public ResponseEntity<Marca> listarPorId(@Validated @RequestBody @PathVariable("id_marca")Integer id_marca)throws Exception{
+        Marca marca3 = service.listaPorId(id_marca);
+        if (marca3 == null){
+            throw new Exception("No existe ID");
+        }
+        return new ResponseEntity<Marca>(marca3, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/eliminar/{id_marca}")
+    public ResponseEntity<Void> aliminarMarca(@Validated @RequestBody @PathVariable("id_marca")Integer id_marca)throws Exception{
+        Marca marca4 = service.listaPorId(id_marca);;
+        if (marca4 == null){
+            throw new Exception("No existe ID");
+        }
+        service.eliminar(id_marca);
+        return new ResponseEntity<Void>(HttpStatus.OK);
     }
 
 }
